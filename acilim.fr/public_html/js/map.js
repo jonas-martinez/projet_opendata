136b418c5e1a0cf8ea3715e5ba9a8fac7709a5ab
Lois Bigot <lois.bigot@etudiant.univ-lr.fr>
2020-10-24 16:46:24 +0200

    Ajout affichage informations communes

--- acilim.fr/public_html/js/map.js
+++ acilim.fr/public_html/js/map.js
@@ -1,21 +1,23 @@
-var start_location = [48.852969,2.349903];
 function init() {
     
     // On initialise la latitude et la longitude de Paris (centre de la carte)
-    
+    // Utilisé si pas de geolocalisation de la part de l'utilisateur
+    var start_location = [48.852969,2.349903];
     var start_zoom = 9
     var macarte = null;
     // On récupère la localisation de l'utilisateur s'il accepte pour qu'elle soit le centre de la carte 
-    // NON FONCTIONNEL
     if(navigator.geolocation){
         function setPosition(pos){
             if (!window.location.href.includes("?")){
                 window.location.href+="?lat="+pos.coords.latitude+"&long="+pos.coords.longitude
             }
         }
-        navigator.geolocation.getCurrentPosition(setPosition)
-        new_location = window.location.search.replace("?","").replace("lat=","").replace("long=","").split("&")
-        start_location=[new_location[0],new_location[1].replace("?","")]
+        // Si l'utilisateur donne la position on modifie 'start_location' pour sa position
+        if (window.location.search){
+            navigator.geolocation.getCurrentPosition(setPosition)
+            new_location = window.location.search.replace("?","").replace("lat=","").replace("long=","").split("&")
+            start_location=[new_location[0],new_location[1].replace("?","")]
+        }
     }
 
     // Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
@@ -28,16 +30,15 @@ function init() {
         maxZoom: 20
     }).addTo(macarte);
     refreshMap(macarte)
-    
-    //var marker = L.marker([lat, lon]).addTo(macarte);
-    // Coordonnées de la carte au chargement
-    
-    
 
-    // Actualisation des villes 
+    // On écoute les évenements de déplacement de la carte (le zoom est un mouvement)
+    macarte.on({'moveend' : moveend,'movestart':movestart});
+    
+    // Fonction d'ctualisation des villes à la fin du déplacement de la carte
     function moveend(e){
         refreshMap(macarte);
     }
+    // Fonction permettant de supprimer tous les layers de la carte dès qu'elle bouge
     function movestart(e){
         $i=0;
         macarte.eachLayer(function(layer){
@@ -48,24 +49,31 @@ function init() {
 
         })
     }
-    macarte.on({'moveend' : moveend,'movestart':movestart});
+
 }
 
+
+// Refresh entièrement la map (visible) à chaque fois qu'elle bouge (même un tout petit peu)
 function refreshMap(map){
-    if (map.getZoom()>8){
+    // On charge les communes uniquement si le zoom de la carte est >= 9
+    if (map.getZoom()>=9){
         var bounds = map.getBounds();
         var coord_map = [[bounds._southWest.lat,bounds._northEast.lng],[bounds._northEast.lat,bounds._northEast.lng],[bounds._northEast.lat,bounds._southWest.lng],[bounds._southWest.lat,bounds._southWest.lng]];
-        var request = "select nom_commune, ST_AsGeoJSON(contours, 9, 4), code_insee from communes where ST_Intersects(ST_SetSRID(contours,4326)::geography, St_GeomFromText(\'POLYGON(("+coord_map[0][1]+" "+coord_map[0][0]+","+coord_map[1][1]+" "+coord_map[1][0]+","+coord_map[2][1]+" "+coord_map[2][0]+","+coord_map[3][1]+" "+coord_map[3][0]+","+coord_map[0][1]+" "+coord_map[0][0]+"))\'));";
+        // On ne charge plus les ville de Paris, Lyon et Marseille (les villes qui ont des arrondissement) à partir d'un certain zoom
+        if(map.getZoom()>=10.5){
+            var request = "select nom_commune, ST_AsGeoJSON(contours, 9, 4), code_insee from communes where nom_commune!=\'Lyon\' and nom_commune!=\'Marseille\' and nom_commune!=\'Paris\' and ST_Intersects(contours::geography, St_GeomFromText(\'POLYGON(("+coord_map[0][1]+" "+coord_map[0][0]+","+coord_map[1][1]+" "+coord_map[1][0]+","+coord_map[2][1]+" "+coord_map[2][0]+","+coord_map[3][1]+" "+coord_map[3][0]+","+coord_map[0][1]+" "+coord_map[0][0]+"))\'));";
+        }else{
+            var request = "select nom_commune, ST_AsGeoJSON(contours, 9, 4), code_insee from communes where ST_Intersects(contours::geography, St_GeomFromText(\'POLYGON(("+coord_map[0][1]+" "+coord_map[0][0]+","+coord_map[1][1]+" "+coord_map[1][0]+","+coord_map[2][1]+" "+coord_map[2][0]+","+coord_map[3][1]+" "+coord_map[3][0]+","+coord_map[0][1]+" "+coord_map[0][0]+"))\'));";
+        }
+        // REQUETE AJAX pour récupérer les contours des communes
         $.post("php/request.php",
             {'request':request}, 
             function(data){
                 result = JSON.parse(data);
-                communes=[];
                 $.each(result,function(_, data){
                     commune = JSON.parse(data[1])
-                    commune.INSEE = data[2]
                     commune.nom=data[0]
-                    //console.log(commune)
+                    commune.INSEE = data[2]
                     oneCommune = L.geoJSON(commune,{
                         style:function(feature){
                             return{
@@ -81,21 +89,20 @@ function refreshMap(map){
                             })
                         }
                     });
-                    // Info à afficher dans la popup au clic sur la commune
-                    oneCommune.bindPopup(data[0]);
                     oneCommune.addTo(map);
-                    communes.push(oneCommune);
                 });
         });
     }
 }
+// Fonction pour surligner la commune au survol
 function communeLayoutMouseOver(e){
     e.target.setStyle({
         color:'#a8a8a8',
         fillOpacity:0.4,
-        weight: 6
+        weight: 5
     })
 }
+// Fonction pour réinitialiser le style des communes (layers)
 function communeReset(e){
     e.target.setStyle({
         color:'rgba(0,0,0,0)',
@@ -104,21 +111,28 @@ function communeReset(e){
 }
 
 function communeClick(e){
-    let insee = e.target.feature.geometry.INSEE
     var nom_ville = e.target.feature.geometry.nom
-    // Récupérer l'année choisis par l'utilisateur
+    var insee = e.target.feature.geometry.INSEE
+    document.getElementById("nomVille").innerHTML=nom_ville
+    document.getElementById("habitantsParAnnee").lastElementChild.innerHTML=""
+    document.getElementById("prixMetreCarre").lastElementChild.innerHTML=""
+    document.getElementById("nbConstruction").lastElementChild.innerHTML=""
+    // Il faut récupérer l'année choisis par l'utilisateur
     var annee=2019
-    let request = "select prixm2, population from prixm2 pr, pop_annee as pop where pr.code_insee=\'"+insee+"\' and pop.code_insee=\'"+insee+"\' and pr.annee="+annee+" and pop.annee="+annee+";";
+    let request = "select prixm2, population, nombre_constructions from prixm2 pr, population_annee pop, constructions c where pr.code_insee=\'"+insee+"\' and pop.code_insee=\'"+insee+"\' and c.code_insee=\'"+insee+"\' and pr.annee="+annee+" and pop.annee="+annee+";";
     $.post("php/request.php",
         {"request":request},
         function(data){
             let result = JSON.parse(data)
+            console.log(result)
             // On modifie le corps html pour mettre à jours la div concernée
-            let nbHabitants = result[0][1]
+            var nbHabitants = result[0][1]
             let prix = result[0][0]+" €"
+            let constructions = result[0][2]
             document.getElementById("nomVille").innerHTML=nom_ville
             document.getElementById("habitantsParAnnee").lastElementChild.innerHTML=nbHabitants
             document.getElementById("prixMetreCarre").lastElementChild.innerHTML=prix
+            document.getElementById("nbConstruction").lastElementChild.innerHTML=constructions
         }
     )
 }
